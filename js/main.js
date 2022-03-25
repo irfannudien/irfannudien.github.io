63906ad982a9fbd8078f96a48f6c7e33be5df184
Irfannudien <muhammadirfannudien@gmail.com>
2022-03-25 17:01:21 +0700

    update three js

--- js/main.js
+++ js/main.js
@@ -167,3 +167,58 @@ $(window).scroll(function () {
     $(".project__container").addClass("show__element");
   }
 });
+
+/*==================== THREE JS ANIMATION ====================*/
+//Variables for setup
+let container;
+let camera;
+let renderer;
+let scene;
+let computer;
+
+function init() {
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+
+  const fov = 30;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 500;
+
+  //Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(-13.25, 0.4, 28);
+
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1.5);
+  light.position.set(50, 50, 100);
+  scene.add(light);
+
+  // //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Model
+  let loader = new THREE.GLTFLoader();
+  loader.load("../assets/computer/scene.gltf", (gltf) => {
+    scene.add(gltf.scene);
+
+    computer = gltf.scene.children[0];
+    animate();
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  computer.rotation.z += 0.005; // speed
+  renderer.render(scene, camera);
+}
+
+init();
